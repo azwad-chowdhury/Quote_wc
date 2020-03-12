6e5d55530fdb890cf6b947c36ac91aa2e716123f
Md. Azwad Hasan Chowdhury <44750724+azwad-chowdhury@users.noreply.github.com>
2020-03-12 16:28:16 +0600

    Add files via upload

---
+++ khabib.py
@@ -0,0 +1,43 @@
+from PIL import Image
+# %matplotlib inline
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+import numpy as np
+
+
+
+# print('Imported')
+
+quote = open('khabib.txt', 'r').read()
+
+print ('File downloaded and saved!')
+
+stopwords = set(STOPWORDS)
+
+# Setting up redundant stopwords
+stopwords.add('Khabib')
+stopwords.add('Nurmagomedov')
+stopwords.add('quote')
+
+khabib = np.array(Image.open('khabib.jpg'))
+
+print('Image downloaded and saved!')
+
+# instantiating a word cloud object
+khabib_wc = WordCloud(
+    background_color='white',
+    max_words=2000,
+    stopwords=stopwords
+)
+
+
+# generating the word cloud
+khabib_wc.generate(quote)
+
+
+# display the word cloud
+plt.imshow(khabib_wc, interpolation='bilinear')
+plt.axis('off')
+plt.savefig('khabib_wc.png',bbox_inches='tight',dpi = 1200)
+plt.show()
